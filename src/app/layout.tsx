0cb562b153da11fcbbc484fe1b199f731c3f3714
Bladen Ward <Bladenward1213@gmail.com>
2024-05-31 18:32:14 -0500

    added topnav w/ signin button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import "~/styles/globals.css";
 
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 import { GeistSans } from "geist/font/sans";
+import Link from "next/link";
 
 export const metadata = {
   title: "Create T3 App",
@@ -18,7 +19,25 @@ export default function RootLayout({
     return (
         <ClerkProvider>
             <html lang="en" className={`${GeistSans.variable}`}>
-                <body>{children}</body>
+                <body>
+                    <div className="grid h-screen grid-rows-[auto,1fr]">
+                        <nav className="flex items-center justify-between p-4 bg-[#2e026d]  text-white">
+                            <Link href="/">
+                                File<span className="text-[hsl(280,100%,70%)] font-bold">Drop</span>
+                            </Link>
+
+                            <div className="flex gap-4">
+                                <SignedIn>
+                                    <UserButton />
+                                </SignedIn>
+                                <SignedOut>
+                                    <SignInButton />
+                                </SignedOut>
+                            </div>
+                        </nav>
+                        <main className="overflow-y-scroll">{children}</main>
+                    </div>
+                </body>
             </html>
         </ClerkProvider>
     );
